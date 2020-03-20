a4b4a932cb1d76e8630f767c38bc524d57675fcb
sbake68 <shawnblake@mail.weber.edu>
2020-03-20 08:59:25 -0600

    Final submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -4,4 +4,17 @@ require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
-//TODO
+
+import "bootstrap"
+
+let f = document.getElementById("myForm")
+
+f.onsubmit = function(){
+    let n = document.getElementById("name-fld").value
+    alert("Welcome " + n)
+}
+
+
+
+
+
